2b486484eb5a2664953ebdd74c593580bb29902e
HumbertoOliveira <humberto.araripe12@gmail.com>
2022-12-14 16:39:25 -0300

    feat: added encoding standards

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from "./app";
 
-app.listen(3333, () =>
-    console.log("🚀 Server is running on http://localhost:3333")
-);
+app.listen(3333, () => {
+    console.log("🚀 Server is running on http://localhost:3333");
+});
