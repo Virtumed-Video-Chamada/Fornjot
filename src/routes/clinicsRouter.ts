818857e0f55a7c3f6d384faea861f1002bdfa180
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-12-15 22:32:13 -0300

    feat(routes): create basic router

---
+++ src/routes/clinicsRouter.ts
@@ -0,0 +1,12 @@
+import { request, response, Router } from 'express';
+
+const clinicsRouter = Router();
+
+clinicsRouter.post('/', (request,response) =>{
+//  const {email, password}
+
+
+
+})
+
+export default clinicsRouter;
