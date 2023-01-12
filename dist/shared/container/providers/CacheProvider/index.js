a788ef662a86cabb749a1d7c869a6161c47c2b8f
LyoDekken <humberto.araripe12@gmail.com>
2023-01-12 20:28:29 -0300

    feat: exclude dist

--- dist/shared/container/providers/CacheProvider/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-var _RedisCacheProvider = _interopRequireDefault(require("./implementations/RedisCacheProvider"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-_tsyringe.container.registerSingleton("CacheProvider", _RedisCacheProvider.default);
--- dist/shared/container/providers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-var _RedisCacheProvider = _interopRequireDefault(require("./CacheProvider/implementations/RedisCacheProvider"));
-require("./StorageProvider");
-require("./MailTemplateProvider");
-require("./MailProvider");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-_tsyringe.container.registerInstance("CacheProvider", _tsyringe.container.resolve(_RedisCacheProvider.default));
