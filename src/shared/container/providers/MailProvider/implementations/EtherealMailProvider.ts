22e9fc1b0f60fef4a354b3ecd1f7058dd348b3f8
LyoDekken <humberto.araripe12@gmail.com>
2023-01-31 20:25:47 -0300

    feat: fix erros

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,50 +1,47 @@
-import nodemailer, { Transporter } from "nodemailer";
-import { injectable, inject } from "tsyringe";
+import nodemailer, { Transporter } from 'nodemailer';
+import { injectable, inject } from 'tsyringe';
 
-import IMailTemplateProvider from "@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider";
-import IMailProvider from "../models/IMailProvider";
-import ISendMailDTO from "../dtos/ISendMailDTO";
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 @injectable()
 export default class EtherealMailProvider implements IMailProvider {
-  private client: Transporter;
+    private client: Transporter;
 
-  constructor(
-    @inject("MailTemplateProvider")
-    private mailTemplateProvider: IMailTemplateProvider,
-  ) {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      auth: {
-        user: "humberto.araripe@gmail.com",
-        pass: "8DfWYV57vJrDF2TEkK",
-      },
-    });
+    constructor(
+        @inject('MailTemplateProvider')
+        private mailTemplateProvider: IMailTemplateProvider,
+    ) {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'humberto.araripe@gmail.com',
+                pass: '8DfWYV57vJrDF2TEkK',
+            },
+        });
 
-    this.client = transporter;
-  }
+        this.client = transporter;
+    }
 
-  public async sendMail({
-    to,
-    from,
-    subject,
-    templateData,
-  }: ISendMailDTO): Promise<void> {
-    const message = await this.client.sendMail({
-      from: {
-        name: from?.name || "Equipe GoBarber",
-        address: from?.email || "equipe@gobarber.com.br",
-      },
-      to: {
-        name: to.name,
-        address: to.email,
-      },
-      subject,
-      html: await this.mailTemplateProvider.parse(templateData),
-    });
-
-    console.log("Message sent: %s", message.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
-  }
+    public async sendMail({
+        to,
+        from,
+        subject,
+        templateData,
+    }: ISendMailDTO): Promise<void> {
+        const message = await this.client.sendMail({
+            from: {
+                name: from?.name || 'humberto',
+                address: from?.email || 'humberto.araripe12@gmail.com',
+            },
+            to: {
+                name: to.name,
+                address: to.email,
+            },
+            subject,
+            html: await this.mailTemplateProvider.parse(templateData),
+        });
+    }
 }
