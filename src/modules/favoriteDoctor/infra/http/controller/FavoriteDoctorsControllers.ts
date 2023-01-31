1711918781fc179621615b0ab6b4ff7cb9fd5574
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2023-01-30 23:27:24 -0300

    fix(favoriteDoctor): refator params

--- src/modules/favoriteDoctor/infra/http/controller/FavoriteDoctorsControllers.ts
+++ src/modules/favoriteDoctor/infra/http/controller/FavoriteDoctorsControllers.ts
@@ -1,23 +1,19 @@
+import CreatefavoriteDoctorsService from '@modules/favoriteDoctor/service/create/CreateFavoriteDoctor';
+import { instanceToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-import { instanceToInstance } from 'class-transformer';
-import CreatefavoriteDoctorsService from '@modules/favoriteDoctor/service/create/CreateFavoriteDoctor';
-import { FavoriteDoctorDto } from '@modules/favoriteDoctor/dtos/ICreateFavoriteDoctorDto';
 
 export default class favoriteDoctorssController {
     public async create(
         request: Request,
         response: Response,
-        data: FavoriteDoctorDto,
     ): Promise<Response> {
-        const { pacient_id, doctor_id } = request.body;
+        const { doctor_id } = request.body;
 
         const createUser = container.resolve(CreatefavoriteDoctorsService);
 
         const user = await createUser.execute({
-            pacient_id,
             doctor_id,
-            data,
         });
 
         return response.json(instanceToInstance(user));
